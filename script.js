3fc406f4bdad56f00cfd71893ba6635523b65c35
Flwzz <111737370+fpbismyname@users.noreply.github.com>
2026-03-12 04:13:29 +0700

    Initialize Swiper instance in script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            breakpoints: {
+                640: { slidesPerView: 2, spaceBetween: 20 },
+                1024: { slidesPerView: 3, spaceBetween: 30 },
+            },
+        });
